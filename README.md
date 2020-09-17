56a26e2263858f05d973ce8ab0184ed4e7214438
Dominikus Baur <dominikus.baur@gmail.com>
2020-09-17 02:22:38 +0200

    v0.900

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# whats-stopping-you-from-coding-like-this
+# What's Stopping You From Coding Like This
+
+A Chrome extension that lets you unleash your inner POWER DEVELOPER.
+
+Since we all know that the only thing that defines our values as human beings and
+provides a reason for our existence is the number of version-managed commits we made
+on a specific website, What's Stopping You From Coding Like This (WSYFCLT) let's you
+improve your commit history so you too can be a valuable member of human society.
+
+![It just works](docs/it-works.png)
+
+## How to use
+
+1. Download the latest release.
+2. Unzip to your favorite folder.
+3. Go to Chrome Extensions [chrome://extensions](chrome://extensions)
+4. Enable 'Developer Mode' on the top-right if you haven't yet (what kind of developer are you even??)
+5. Click on 'Load Unpacked' and navigate to your favorite folder.
+6. Go to your Github.com profile - you should see a powerful purple flex icon in your extension toolbar.
+7. Click it and select appropriate skill level:
+![do it](docs/popup.png)
+8. Bask in your own glory 🤩
---
+++ src/popup.js
@@ -0,0 +1,52 @@
+const code = `function startDeveloping(level) {
+  console.log('internal:' + level);
+  const DEFAULT_CONTRIBUTION_SIZE = 38;
+  const colorScale = ["#ebedf0", "#9be9a8", "#40c463", "#30a14e", "#216e39"];
+
+  const allBoxes = document.querySelectorAll("rect.day");
+  allBoxes.forEach((box) => {
+    let contributionColor = Math.round(Math.random());
+    contributionColor = Math.min(
+      colorScale.length - 1,
+      contributionColor + level
+    );
+    box.setAttribute("fill", colorScale[contributionColor]);
+  });
+
+  const possibleContributionLabels = document.querySelectorAll(
+    "h2.text-normal"
+  );
+  possibleContributionLabels.forEach((label) => {
+    const t = label.innerText;
+    if (t.indexOf("contributions in") !== -1) {
+      // split off leading number:
+      let parts = t.split(" ");
+      parts[0] = Math.floor(
+          (Math.random() + 0.75) *
+          allBoxes.length *
+          Math.pow((level + 1), 2) *
+          DEFAULT_CONTRIBUTION_SIZE
+      );
+      label.innerText = parts.join(" ");
+    }
+  });
+};`;
+
+let developButtons = document.querySelectorAll(".develop-button");
+
+console.log("yo");
+console.log(developButtons);
+
+developButtons.forEach((button, i) => {
+  button.onclick = () => {
+    console.log("external: " + i);
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.executeScript(tabs[0].id, {
+        code: code + "startDeveloping(" + i + ")",
+      });
+    });
+
+    window.close();
+  };
+});
